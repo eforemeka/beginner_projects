9992624b7b8a5772594f9fd8b2e906bfa953b8c0
Benny <93387437+eforemeka@users.noreply.github.com>
2021-11-14 23:14:12 +0100

    "I just finished the Guess a Number Game. I am making this commit on VSCode for a change lol"

---
+++ GuessTheNumber.py
@@ -0,0 +1,18 @@
+#Import the random module. This makes various funtions under the random package accessible in our code
+import random
+#Define my function with parameter x where x is the top interval and 1 is the bottom interval of our range
+def GuessNumber(x):
+    random_number = random.randint(1, x)                                                                                             
+    guess = 0
+#The while loop is used here so that the user can keep guessing numbers until they are able to guess the
+#right number. The if statement is there to let the user know how close they are to the right answer
+
+    while guess != random_number:
+        guess = int(input("Guess a number my gee: "))
+        if guess > random_number:
+            print("Nope. Guess lower")
+        elif guess < random_number:
+            print ("Nope. Guess Higher")
+    print(f"Spot on! You guessed {random_number} and that was right!")
+
+GuessNumber(10)
