5b5b52928bde904a3f3b686d4211606492a002a7
Benny <93387437+eforemeka@users.noreply.github.com>
2021-11-18 22:33:58 +0100

    "I need to revisit this code because I am not impressed at all"

--- GuessTheNumber2.py
+++ GuessTheNumber2.py
@@ -1,3 +1,24 @@
 import random
 
-def Guess_Number()
+#This function allows the computer to guess a number you have picked
+
+def computer_guess(x):
+    low = 1 
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        #guess = random.randint(low, high)
+        feedback = input(f"Is {guess} higher (H), lower (L), or correct (C)? ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"I guessed your number, {guess}, correctly peasant, therefore I am superior!")
+    
+
+computer_guess(10)
